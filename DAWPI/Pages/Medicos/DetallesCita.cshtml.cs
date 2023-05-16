045ddcfbe5c8add9ae0305a6f9e177026ef82e2e
JUANK\jcbc2 <csi20-jbadrbo@colegioaltair.net>
2023-05-16 23:47:22 +0200

    Terminar asignar fecha y hora manejo errores y seguir con el anterior commit.

--- DAWPI/Pages/Medicos/DetallesCita.cshtml.cs
+++ DAWPI/Pages/Medicos/DetallesCita.cshtml.cs
@@ -2,9 +2,13 @@ using DAL.DAOaDTO;
 using DAL.DTO;
 using DAL.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Data;
+using Org.BouncyCastle.Asn1.Cms;
+using System;
+using System.Globalization;
+using System.Linq;
 
 namespace DAWPI.Pages.Medicos
 {
@@ -17,7 +21,14 @@ namespace DAWPI.Pages.Medicos
             _db = db;
         }
 
+        [BindProperty]
         public CitaDTO Cita { get; set; }
+
+        [BindProperty]
+        public string fecha { get; set; }
+
+        [BindProperty]
+        public string hora { get; set; }
 
         public void OnGet()
         {
@@ -29,5 +40,51 @@ namespace DAWPI.Pages.Medicos
                 Cita = citaDTO;
             }
         }
+
+        public IActionResult OnPostAsignar()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            string emailUsuario = User.FindFirst("EmailUsuario")?.Value;
+            int? detalle = HttpContext.Session.GetInt32("detalle");
+            if (detalle.HasValue)
+            {
+                Cita cita = _db.Citas.FirstOrDefault(c => c.Id == detalle);
+                if (cita != null)
+                {
+                    DateOnly fechaCita = DateOnly.ParseExact(fecha, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                    TimeOnly horaCita = TimeOnly.ParseExact(hora, "HH:mm", CultureInfo.InvariantCulture);
+
+                    // Obtener las citas del médico para la misma fecha
+                    List<Cita> citasMedico = _db.Citas
+                        .Where(c => c.NombreMedico == emailUsuario && c.Fecha == fechaCita)
+                        .ToList();
+
+
+                    foreach (Cita citaMedico in citasMedico)
+                    {
+                        TimeOnly horaAbajo = citaMedico.Hora.Value.AddMinutes(30);
+                        TimeOnly horaArriba = citaMedico.Hora.Value.AddMinutes(30);
+
+                        if (horaCita.IsBetween(horaAbajo, horaArriba))
+                        {
+                            ModelState.AddModelError("", "La hora seleccionada coincide con otra cita del médico.");
+                            return Page();
+                        }
+                    }
+
+                    cita.Fecha = fechaCita;
+                    cita.Hora = horaCita;
+                    cita.EstadoCita = "A";
+                    _db.SaveChanges();
+                }
+            }
+
+            return RedirectToPage("/Medicos/Citas");
+        }
+
     }
 }
